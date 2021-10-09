b9190394fe8a5ad829215c402d47b8396797d4b6
abdalla986 <moabab4@gmail.com>
2021-10-09 20:47:40 +0200

    update

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,7 @@
+import { checkForName } from './js/nameChecker'
+import { handleSubmit } from './js/formHandler'
+import { json } from 'express'
+
+console.log(checkForName);
+
 alert("I EXIST!")
--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,28 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-    entry: './src/client/index.js',
+  mode: 'development',
+  entry: './src/client/index.js',
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: '/\.js$',
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
 }
+
+{
+	// resolve: {
+	// 	extensions: ['.cson']
+	// },
+	module: {
+		loaders: [
+			{ test: /\.cson$/, loader: "cson" }
+		]
+	}
+}
+
